00cad7ac5e7c94712e2680266b239e5bfa82b011
William Jonathan <willyeager01@gmail.com>
2021-08-16 11:55:59 -0300

    spring boot init

---
+++ src/test/java/br/com/fcamara/acheiaquiapi/AcheiaquiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fcamara.acheiaquiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcheiaquiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
