9fe565b444b2d03b499b42ca2d56f96de86fe7b8
William Jonathan <willyeager01@gmail.com>
2021-08-16 11:58:52 -0300

    spring boot init

---
+++ src/main/java/br/com/fcamara/acheiaquiapi/AcheiaquiApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fcamara.acheiaquiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcheiaquiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcheiaquiApiApplication.class, args);
+	}
+
+}
