a54f527922dccca93d2e48e25b11424e32a36432
William Jonathan <willyeager01@gmail.com>
2021-08-27 02:03:57 -0300

    feat: swagger authorization

--- src/main/java/br/com/fcamara/acheiaquiapi/config/swagger/SwaggerConfiguration.java
+++ src/main/java/br/com/fcamara/acheiaquiapi/config/swagger/SwaggerConfiguration.java
@@ -1,17 +1,24 @@
 package br.com.fcamara.acheiaquiapi.config.swagger;
 
 import br.com.fcamara.acheiaquiapi.model.authentication.Usuario;
+import com.google.common.net.HttpHeaders;
+import io.swagger.models.auth.In;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Parameter;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfiguration {
@@ -24,13 +31,24 @@ public class SwaggerConfiguration {
                 .paths(PathSelectors.ant("/**"))
                 .build()
                 .ignoredParameterTypes(Usuario.class)
-                .globalOperationParameters((Arrays.asList(
-                        new ParameterBuilder()
-                                .name("Autorization")
-                                .description("Header para token JWT")
-                                .modelRef(new ModelRef("string"))
-                                .parameterType("header")
-                                .required(false)
-                                .build())));
+                .securitySchemes(Arrays.asList(new ApiKey("Bearer Token Acess", HttpHeaders.AUTHORIZATION, In.HEADER.name())))
+                .securityContexts(Arrays.asList(securityContext()));
     }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.ant("/contatos"))
+                .forPaths(PathSelectors.ant("/contatos/**"))
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("Contato", "acessaAListaOposta");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("Token", authorizationScopes));
+    }
+
 }
