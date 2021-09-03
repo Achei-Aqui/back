d5c5e0ce6653a57c326ab0961814ef897c2e9674
William Jonathan <willyeager01@gmail.com>
2021-09-03 08:01:42 -0300

    cors annotation

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM openjdk:16-slim
+MAINTANER William Jonathan
 RUN addgroup --system  spring && adduser --system spring --ingroup spring
 USER spring:spring
 ARG JAR_FILE=target/*.jar
--- src/main/java/br/com/fcamara/acheiaquiapi/controller/contato/ContatosController.java
+++ src/main/java/br/com/fcamara/acheiaquiapi/controller/contato/ContatosController.java
@@ -31,6 +31,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/contatos")
 public class ContatosController {
